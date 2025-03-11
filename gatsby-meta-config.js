c85fdbbbeeee7b85dd0a0bc0a6817d01e5818ee3
Joshuagermany <101688691+Joshuagermany@users.noreply.github.com>
2025-03-11 11:48:48 +0900

    Update gatsby-meta-config.js
    change profile

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,26 +1,26 @@
 module.exports = {
-  title: `zoomkoding.com`,
-  description: `줌코딩의 개발일기`,
+  title: `joshuagermany.com`,
+  description: `josh의 개발일기`,
   language: `ko`, // `ko`, `en` => currently support versions for Korean and English
-  siteUrl: `https://www.zoomkoding.com`,
+  siteUrl: `https://www.joshuagermany.com`,
   ogImage: `/og-image.png`, // Path to your in the 'static' folder
   comments: {
     utterances: {
-      repo: ``, // `zoomkoding/zoomkoding-gatsby-blog`,
+      repo: `joshuagermany/joshuagermany-gatsby-blog`, // `zoomkoding/zoomkoding-gatsby-blog`,
     },
   },
   ga: '0', // Google Analytics Tracking ID
   author: {
-    name: `정진혁`,
+    name: `조승찬`,
     bio: {
       role: `개발자`,
-      description: ['사람에 가치를 두는', '능동적으로 일하는', '이로운 것을 만드는'],
+      description: ['사람에 가치를 두는', '계속해서 도전하는', '이로운 것을 만드는'],
       thumbnail: 'sample.png', // Path to the image in the 'asset' folder
     },
     social: {
-      github: ``, // `https://github.com/zoomKoding`,
-      linkedIn: ``, // `https://www.linkedin.com/in/jinhyeok-jeong-800871192`,
-      email: ``, // `zoomkoding@gmail.com`,
+      github: `https://github.com/joshuagermany`, // `https://github.com/zoomKoding`,
+      linkedIn: `https://www.linkedin.com/in/seungchan-cho-805991340/`, // `https://www.linkedin.com/in/jinhyeok-jeong-800871192`,
+      email: `chosc0417@gmail.com`, // `zoomkoding@gmail.com`,
     },
   },
 
@@ -75,11 +75,11 @@ module.exports = {
         title: '개발 블로그 테마 개발',
         description:
           '개발 블로그를 운영하는 기간이 조금씩 늘어나고 점점 많은 생각과 경험이 블로그에 쌓아가면서 제 이야기를 담고 있는 블로그를 직접 만들어보고 싶게 되었습니다. 그동안 여러 개발 블로그를 보면서 좋았던 부분과 불편했던 부분들을 바탕으로 레퍼런스를 참고하여 직접 블로그 테마를 만들게 되었습니다.',
-        techStack: ['gatsby', 'react'],
+        techStack: ['AI', 'react'],
         thumbnailUrl: 'blog.png',
         links: {
-          post: '/gatsby-starter-zoomkoding-introduction',
-          github: 'https://github.com/zoomkoding/zoomkoding-gatsby-blog',
+          post: '/gatsby-starter-joshuagermany-introduction',
+          github: 'https://github.com/joshuagermany/joshuagermany-gatsby-blog',
           demo: 'https://www.zoomkoding.com',
         },
       },
